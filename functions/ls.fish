dc87b4a90d6ed0739cde17fa769d8304e8a1aaf6
graham <graham.annett@gmail.com>
2024-10-12 16:05:08 -0600

    Refactor ls.fish to use EZA_STANDARD_OPTIONS

--- functions/ls.fish
+++ functions/ls.fish
@@ -1,5 +1,6 @@
 function ls --wraps='/opt/homebrew/bin/eza --group --header --group-directories-first' --description 'alias ls=/opt/homebrew/bin/eza --group --header --group-directories-first'
     # note: original command is /bin/ls
     # i think this is the quickest way to replace the `ls` command with `eza`
-    /opt/homebrew/bin/eza --group --header --group-directories-first $argv
+    # $EZA_STANDARD_OPTIONS is equivalent to: `--group --header --group-directories-first`
+    /opt/homebrew/bin/eza $EZA_STANDARD_OPTIONS $argv
 end
