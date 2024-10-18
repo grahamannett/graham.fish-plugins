41ba23025a57d54089c50da3c65c65f2366f773d
graham <graham.annett@gmail.com>
2024-10-18 11:28:46 -0600

    Refactor avenv.fish to improve virtual environment activation and deactivation

--- functions/avenv.fish
+++ functions/avenv.fish
@@ -9,15 +9,9 @@
 ## * Update syntax to work with new versions of fish.
 
 
-# Function to deactivate the current virtual environment
-# function deactivate_virtualenv
-#     if type deactivate >/dev/null 2>&1
-#         deactivate
-#     end
-# end
 
 function avenv --description "Activate/Deactivate virtualenv"
-    status is-command-substitution && echo 'early exit' && return
+    status is-command-substitution && return
 
     # Searched directories are the current directory, and the root of the current git repo if applicable
     set __cdirs (pwd)
@@ -26,12 +20,20 @@ function avenv --description "Activate/Deactivate virtualenv"
     end
 
     # Scan directories for a fish-compatible virtual environment
-    set -l VENV_DIR_NAMES env .env venv .venv
+    set -l activate_script "bin/activate.fish"
+    set -l VENV_DIR_NAMES .venv venv .env env
     set -l found_venv ""
     for venv_dir in $__cdirs/$VENV_DIR_NAMES
-        if test -e "$venv_dir/bin/activate.fish"
+        if test -e "$venv_dir/$activate_script"
             set found_venv $venv_dir
             break
+        end
+    end
+
+    function __try_deactivate
+        # alt is to use `test -n $VIRTUAL_ENV`
+        if type -q deactivate
+            deactivate
         end
     end
 
@@ -39,19 +41,15 @@ function avenv --description "Activate/Deactivate virtualenv"
     if test -n "$found_venv"
         if test "$VIRTUAL_ENV" != "$found_venv"
             # Deactivate the current virtual environment before activating a new one
-            if test -n "$VIRTUAL_ENV"
-                deactivate
-            end
-            # echo "Activating virtualenv at $found_venv"
-            # Source the activate.fish script
-            source "$found_venv/bin/activate.fish"
+            __try_deactivate
+            source "$found_venv/$activate_script"
         else
-            # echo "Virtualenv at $found_venv is already active"
+            # here the current virtual_env is the same as the found one
+            # in this case deactivate if deactivate is a cmd
+            __try_deactivate
         end
     else
         # Deactivate if a virtual environment is currently active but no env found
-        if test -n "$VIRTUAL_ENV"
-            deactivate
-        end
+        __try_deactivate
     end
 end
